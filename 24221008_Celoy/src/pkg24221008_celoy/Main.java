0f8ba53c653fb275f0a83b0ee6199be5678063d5
ChelseaBolagi <celytendean7@gmail.com>
2025-05-07 15:01:11 +0700

    Add files via upload
    Praktikum1 AlPro

---
+++ 24221008_Celoy/src/pkg24221008_celoy/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg24221008_celoy;
+
+/**
+ *
+ * @author LAURENA
+ */
+public class Main {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int nim = 24221008;
+       String nama = "Celoy";
+       String alamat = "Sulawesi Tengah Kab Parimo Kec Tada";
+       String TTL = "Kasimbar, 30 Desember 2005";
+       
+       
+        System.out.println("BioData");
+       System.out.println("NIM = "+nim);
+       System.out.println("NAMA = "+nama);
+       System.out.println("ALAMAT = "+alamat);
+       System.out.println("Tempat Tanggal Lahir = "+TTL);
+        
+           }
+    
+}
